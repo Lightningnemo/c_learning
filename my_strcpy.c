e15c10bf66ee5c9bc4187d15667d6db6a9d0c09d
Lightningnemo <37526982+Lightningnemo@users.noreply.github.com>
2022-02-26 20:07:34 +0800

    Create my_strcpy.c
    完整版字符串拷贝代码

---
+++ my_strcpy.c
@@ -0,0 +1,13 @@
+char* my_strcpy(char* dest, const char* src)
+{
+	char* ret = dest;
+	assert(dest != NULL);//断言
+	assert(src != NULL);//断言
+
+	while (*dest++ = *src++)
+	{
+      ;
+	}
+	//*dest = *src; //'\0;'
+	return ret;
+}
